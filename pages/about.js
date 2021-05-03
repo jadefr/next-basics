4ea20d09d79a25ce76e564df142973cc14352c35
Jade Silva Ferreira <jade.ferreira@inatel.br>
2021-05-03 11:20:12 -0300

    Create navbar as a component

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>About</title>
+            </Head>
+
+            <h1>About</h1>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore et quo reiciendis repudiandae rerum totam.</h2>
+        </div>
+    )
+}
+
+export default about
